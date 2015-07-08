fbdd0c30ae581620f54277c9ebfdf67e6c78ad49
Selin Dag <sdag@codeaurora.org>
2015-07-08 16:00:37 -0700

    [qca-nss-drv] Add IPv6 reassembly support
    Add IPv6 reassembly support and statistics.
    
    CRs-Fixed: 863422
    
    Change-Id: Idc665281365bd425744cdc459c88ee1eaaa56b1b
    Signed-off-by: Selin Dag <sdag@codeaurora.org>

--- exports/nss_api_if.h
+++ exports/nss_api_if.h
@@ -80,7 +80,7 @@
 #define NSS_MAX_PHYSICAL_INTERFACES 4
 #define NSS_MAX_VIRTUAL_INTERFACES 16
 #define NSS_MAX_TUNNEL_INTERFACES 4
-#define NSS_MAX_SPECIAL_INTERFACES 26
+#define NSS_MAX_SPECIAL_INTERFACES 30
 #define NSS_MAX_DYNAMIC_INTERFACES 32
 
 /**
@@ -119,8 +119,9 @@
 #define NSS_GRE_REDIR_INTERFACE (NSS_SPECIAL_IF_START + 21) /* Interface Number for GRE REDIR base interface */
 #define NSS_LSO_RX_INTERFACE (NSS_SPECIAL_IF_START + 22) /* Interface number for lso */
 #define NSS_SJACK_INTERFACE (NSS_SPECIAL_IF_START + 23) /* Interface Number for GRE REDIR base interface */
-#define NSS_IPV4_REASM_INTERFACE (NSS_SPECIAL_IF_START + 24) /* Special IF number for IPv4 */
+#define NSS_IPV4_REASM_INTERFACE (NSS_SPECIAL_IF_START + 24) /* Special IF number for IPv4 reassembly*/
 #define NSS_DEBUG_INTERFACE (NSS_SPECIAL_IF_START + 25) /* Special IF number for debug interface */
+#define NSS_IPV6_REASM_INTERFACE (NSS_SPECIAL_IF_START + 29) /* Special IF number for IPv6 reassembly */
 
 /**
  * This macro converts format for IPv6 address (from Linux to NSS)
