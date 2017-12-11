5186a2bf8ef5beb23ec1e7853bb0573622cfa8ff
Aniruddha Paul <paulani@codeaurora.org>
2017-12-11 15:57:32 +0530

    [qca-nss-drv] Send the security type at VAP
    Change-Id: I231084cf2bd727bf44350630d7aa332c1af24e85
    Signed-off-by: Aniruddha Paul <paulani@codeaurora.org>

--- exports/nss_wifi_vdev.h
+++ exports/nss_wifi_vdev.h
@@ -147,10 +147,10 @@ enum nss_wifi_vdev_cmd {
 	NSS_WIFI_VDEV_CFG_WNM_CAP_CMD,	/**< Configuration to set wireless network management (WNM) capability on VAP. */
 	NSS_WIFI_VDEV_CFG_WNM_TFS_CMD,	/**< Configuration to set WNM traffic filtering and sleep mode (TFS) capability on VAP. */
 	NSS_WIFI_VDEV_CFG_WDS_EXT_ENABLE_CMD,
-					/**< Configuration to set WDS extention capability on VAP. */
-	NSS_WIFI_VDEV_CFG_WDS_CMD,	/**< Configuration to set WDS on VAP. */
-	NSS_WIFI_VDEV_CFG_AP_BRIDGE_CMD,
-					/**< Configuration to enable/disable client isolation. */
+						/**< Configuration to set WDS extention capability on VAP. */
+	NSS_WIFI_VDEV_CFG_WDS_CMD,		/**< Configuration to set WDS on VAP. */
+	NSS_WIFI_VDEV_CFG_AP_BRIDGE_CMD,        /**< Configuration to enable/disable client isolation. */
+	NSS_WIFI_VDEV_SECURITY_TYPE_CMD,	/**< Configuration to set security type per VAP. */
 	NSS_WIFI_VDEV_MAX_CMD
 };
 
