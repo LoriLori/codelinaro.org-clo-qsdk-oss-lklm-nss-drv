f58be7d076bbf55f91830cf807162393ed88c899
repo sync -j8 -qc --no-tags cd qsdk make package/symlinks cp qca/configs/qsdk/ipq_premium.config .config echo 'CONFIG_TARGET_ipq_ipq807x_64=y'  .config echo 'CONFIG_TARGET_ipq_ipq807x_64_QSDK_Premium=y'  .config make defconfig make V=s -j8 <jbockus@codeaurora.org>
2018-02-27 13:09:22 -0800

    [qca-nss-drv] Moved project to separate subsystem.
    Change-Id: Ibd132fa74968c225a0f1a0dc1ffe00b4d22e6a77
    Signed-off-by: Jackson Bockus <jbockus@codeaurora.org>

--- exports/nss_project.h
+++ exports/nss_project.h
@@ -1,6 +1,6 @@
 /*
  **************************************************************************
- * Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -23,7 +23,7 @@
 #define __NSS_PROJECT_H
 
 /**
- * @addtogroup nss_dscp2pri_subsystem
+ * @addtogroup nss_project_subsystem
  * @{
  */
 
