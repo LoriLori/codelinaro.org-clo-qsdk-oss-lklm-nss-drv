ab1f192acf19284379d13cb6b2ef8189d3d14144
Sakthi Vignesh Radhakrishnan <sradhakr@codeaurora.org>
2018-10-01 10:52:32 -0700

    [qca-nss-drv] Add fast_mem stats to CAPWAP.
    This stats tells if the CAPWAP tunnel has been allocated in
    fast or slow memory.
    
    Change-Id: I2d8f59318e5d21c7d1606e8694df1435129a6992
    Signed-off-by: Sakthi Vignesh Radhakrishnan <sradhakr@codeaurora.org>

--- exports/nss_capwap.h
+++ exports/nss_capwap.h
@@ -128,6 +128,9 @@ struct nss_capwap_stats_msg {
 			/**< Packets dropped because of a DTLS packet. */
 	uint32_t tx_dropped_nwireless;
 			/**< Packets dropped because the nwireless field information is wrong. */
+
+	uint32_t fast_mem;
+			/**< Set to 1 when tunnel is operating in fast memory. */
 };
 
 /**
@@ -346,6 +349,9 @@ struct nss_capwap_tunnel_stats {
 			/**< Packets dropped because of a DTLS packet. */
 	uint64_t tx_dropped_nwireless;
 			/**< Packets dropped because the nwireless field information is wrong. */
+
+	uint32_t fast_mem;
+			/**< Set to 1 when tunnel is operating in fast memory. */
 };
 
 /**
