a7368f5ed9fc3f8429434bbc6db8135032461794
Shivani Soni <soni@codeaurora.org>
2021-07-14 16:42:37 +0530

    [qca-nss-drv] Add support for sending panic/pktlog debug event to host
    Change-Id: I5d93b7f8c451c1161289e1ffa0549c6c58477539
    Signed-off-by: Shivani Soni <soni@codeaurora.org>

--- exports/nss_wifili_if.h
+++ exports/nss_wifili_if.h
@@ -243,6 +243,9 @@ enum nss_wifili_msg_types {
 	NSS_WIFILI_PDEV_TX_CAPTURE_MSG,
 	NSS_WIFILI_PEER_TX_CAPTURE_MSG,
 	NSS_WIFILI_PDEV_V3_RX_ERROR_STATS_SYNC_MSG,
+	NSS_WIFILI_SEND_DBG_LATENCY_EVENT_MSG,
+	NSS_WIFILI_SET_TX_LATENCY_THRESHOLD,
+	NSS_WIFILI_SET_TX_LATENCY_PKTLOG_THRESHOLD,
 	NSS_WIFILI_MAX_MSG
 };
 
@@ -1813,6 +1816,28 @@ struct nss_wifili_mesh_capability_info {
 };
 
 /**
+ * struct nss_wifili_dbg_latency_event_msg
+ * 	Wifili debug latency event sent to host.
+ */
+struct nss_wifili_dbg_latency_event_msg {
+	uint32_t tid;		/**< Traffic Identifier (TID) value. */
+	uint32_t pktlog_en;	/**< Packet log enable value. */
+	uint32_t value;		/**< Packet log enumeration value. */
+	uint32_t tx_latency;	/**< Transmit latency value of event. */
+	uint16_t peer_id;	/**< Peer ID of event. */
+	uint8_t reserved;	/**< Reserved field. */
+};
+
+/*
+ * nss_wifili_set_tx_latency_threshold
+ * 	Sets transmit latency threshold value.
+ */
+struct nss_wifili_set_tx_latency_threshold {
+	uint32_t radio_id;		/**< Radio ID of event. */
+	uint32_t latency_threshold;	/**< Latency threshold value.. */
+};
+
+/**
  * nss_wifili_msg
  *	Structure that describes wifili messages.
  */
@@ -1902,6 +1927,10 @@ struct nss_wifili_msg {
 				/**< Wifili peer Tx capture message. */
 		struct nss_wifili_pdev_v3_error_stats_sync_msg v3_rx_error_stats_msg;
 				/**< Wifili version 3 Rx error statistics message. */
+		struct nss_wifili_dbg_latency_event_msg dbg_info;
+				/**< Debug latency event for host. */
+		struct nss_wifili_set_tx_latency_threshold tx_max;
+				/**< Tx latency threshold value. */
 	} msg;			/**< Message payload. */
 };
 
